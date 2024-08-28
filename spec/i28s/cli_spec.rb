e7bae206d60213e17374c9139109f012fc077e20
Adrien Siami <adrien@siami.fr>
2024-08-28 12:59:05 +0200

    Remove default failing spec

--- spec/i28s/cli_spec.rb
+++ spec/i28s/cli_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Yamlfish::Cli do
   it "has a version number" do
     expect(Yamlfish::Cli::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
